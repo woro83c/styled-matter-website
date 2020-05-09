715a7979b173f74fb591772487f98d7b725b9227
Michael Worobec <eekumu1@hotmail.com>
2020-05-09 15:53:09 +1000

    title and favicons

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,20 @@ export default class App extends NextApp {
         <MDXProvider components={components}>
           <ThemeProvider theme={theme}>
             <Head>
+              <title>Styled Matter - One bit of UI to style them all</title>
+
+              {/* Favicons */}
+              <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+              <link rel="icon" type="image/svg+xml" sizes="any" href="/favicon.svg" />
+              <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+              <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+              <link rel="manifest" href="/site.webmanifest" />
+              <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+              <meta name="msapplication-TileColor" content="#ffffff" />
+              <meta name="theme-color" content="#ffffff" />
+
+              {/* Fonts */}
+              <link crossOrigin="" href="https://fonts.gstatic.com" rel="preconnect" />
               <link
                 href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;900&family=Roboto+Mono&display=swap"
                 rel="stylesheet"
